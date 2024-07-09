d404d772e578f049558382ac6a7fa62fac0242dc
Oleksandra Ubska <alessandraubska@gmail.com>
2024-07-09 16:26:21 +0200

    add store.js

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,25 @@
+<script>
+import SingleCharacter from './SingleCharacter.vue'
+
+export default {
+    name: 'CharactersList',
+    components: {
+        SingleCharacter,
+    }
+}
+</script>
+
+<template>
+    <section class="container">
+        <div>
+            <div>
+                <!-- componente di card -->
+                 <SingleCharacter/>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+@use '../style/general.scss' as *
+</style>
---
+++ src/components/SingleCharacter.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'SingleCharacter'
+}
+</script>
+
+<template>
+    <div>
+        <img src="https://i0.wp.com/giornalinocorbino.altervista.org/wp-content/uploads/2020/02/1_d5g31wvUpmILqzge216Fug-1.png?resize=840%2C939" alt="testo alternativo">
+        <h4>Nome card</h4>
+        <div>Status card</div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/general.scss' as *
+</style>
