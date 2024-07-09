0e9ad4b18b79384db5287178cfe60c279f17007c
Oleksandra Ubska <alessandraubska@gmail.com>
2024-07-09 16:02:31 +0200

    add header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: { message: String }
+}
+</script>
+
+<template>
+    <header class="container">
+        <h1>{{ message }}</h1>
+    </header>
+</template>
+
+<style scoped lang="scss">
+h1{
+    font-weight: 700;
+}
+</style>
